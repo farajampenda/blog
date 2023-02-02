02edcdc38d500417ae5b05b29e695b3202f00433
farajampenda <swediroben@gmail.com>
2023-02-02 11:09:12 +0200

    commit

---
+++ src/main/java/com/malu/blog/controller/PostController.java
@@ -0,0 +1,8 @@
+@RestController
+public class PostController {
+    
+    @GetMapping("/dates")
+    public String name() {
+        return "nom:faraja";
+    }
+}
---
+++ src/test/java/com/malu/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
